0ae8b2b4d2286320457bd9b6ec02c0e8267b44f6
maydayv7 <maydayv7@gmail.com>
2026-04-03 22:59:49 +0530

    HAB + SMC Alerts

--- server/v1/modules/alert/alertRoute.js
+++ server/v1/modules/alert/alertRoute.js
@@ -2,10 +2,13 @@
 
 const express = require("express");
 const router = express.Router();
-const { authenticateJWT, authenticateAdminJWT } = require("../../middleware/authenticateJWT");
+const {
+  authenticateJWT,
+  authenticateHabOrSMCJWT,
+} = require("../../middleware/authenticateJWT");
 const { createAlert, getAlerts } = require("./alertController");
 
-router.post("/create", authenticateAdminJWT, createAlert);
+router.post("/create", authenticateHabOrSMCJWT, createAlert);
 router.get("/", authenticateJWT, getAlerts);
 
 module.exports = router;
