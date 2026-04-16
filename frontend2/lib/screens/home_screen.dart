e46d83b1c0f867f741ff15682c598ad004bb4021
aditya-samal <samaladitya2004@gmail.com>
2026-04-16 13:38:23 +0530

    Rain thing fixed.

--- frontend2/lib/screens/home_screen.dart
+++ frontend2/lib/screens/home_screen.dart
@@ -493,7 +493,7 @@ class _HomeScreenState extends State<HomeScreen> with WidgetsBindingObserver {
 
   void _startWeatherBackgroundTicker() {
     _weatherBackgroundTimer?.cancel();
-    _weatherBackgroundTimer = Timer.periodic(const Duration(minutes: 1), (_) {
+    _weatherBackgroundTimer = Timer.periodic(const Duration(seconds: 10), (_) {
       if (!mounted) return;
       _loadWeatherBackground();
     });
